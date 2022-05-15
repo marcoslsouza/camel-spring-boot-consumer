65ef77c78b8888a67b7e00c882acbcc0d949d907
marcoslsouza <marcoslsouzadf@gmail.com>
2022-05-15 05:51:19 -0300

    first commit

---
+++ src/main/java/com/github/marcoslsouza/camel_spring_boot_consumer/CamelSpringBootConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcoslsouza.camel_spring_boot_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelSpringBootConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSpringBootConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcoslsouza/camel_spring_boot_consumer/CamelSpringBootConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcoslsouza.camel_spring_boot_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSpringBootConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
